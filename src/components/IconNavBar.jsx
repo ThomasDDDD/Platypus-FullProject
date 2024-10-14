8994f36576a60067bd6513aa9d236202483e0bca
Julian P <julian-peukert@web.de>
2024-10-14 14:09:09 +0200

    changed LinkPatch

--- src/components/IconNavBar.jsx
+++ src/components/IconNavBar.jsx
@@ -10,67 +10,67 @@ import "./IconNavBar.css"
 export const IconNavBar = () => {
   return (
     <nav className="navbar">
-      <Link to="/category/esszimmer">
+      <Link to="/CategoryPage/esszimmer">
         <div className="icon-container">
           <IoIosStarOutline className="icons"/>
           <p>Esszimmer</p>
         </div>
       </Link>
-      <Link to="/category/küche">
+      <Link to="/CategoryPage/küche">
         <div className="icon-container">
           <PiChair className="icons"/>
           <p>Küche</p>
         </div>
       </Link>
-      <Link to="/category/schlafzimmer">
+      <Link to="/CategoryPage/schlafzimmer">
         <div className="icon-container">
           <MdOutlineTableRestaurant className="icons"/>
           <p>Schlafzimmer</p>
         </div>
       </Link>
-      <Link to="/category/kinderzimmer">
+      <Link to="/CategoryPage/kinderzimmer">
         <div className="icon-container">
           <RiArmchairLine className="icons"/>
           <p>Kinderzimmer</p>
         </div>
       </Link>
-      <Link to="/category/babyzimmer">
+      <Link to="/CategoryPage/babyzimmer">
         <div className="icon-container">
           <IoBedOutline className="icons"/>
           <p>Babyzimmer</p>
         </div>
       </Link>
-      <Link to="/category/flur">
+      <Link to="/CategoryPage/flur">
         <div className="icon-container">
           <LuLampDesk className="icons"/>
           <p>Flur</p>
         </div>
       </Link>
-      <Link to="/category/homeoffice">
+      <Link to="/CategoryPage/homeoffice">
         <div className="icon-container">
           <LuLampDesk className="icons"/>
           <p>Homeoffice</p>
         </div>
       </Link>
-      <Link to="/category/accessoires">
+      <Link to="/CategoryPage/accessoires">
         <div className="icon-container">
           <LuLampDesk className="icons"/>
           <p>Accessoires</p>
         </div>
       </Link>
-      <Link to="/category/wohnzimmer">
+      <Link to="/CategoryPage/wohnzimmer">
         <div className="icon-container">
           <LuLampDesk className="icons"/>
           <p>Wohnzimmer</p>
         </div>
       </Link>
-      <Link to="/category/keller">
+      <Link to="/CategoryPage/keller">
         <div className="icon-container">
           <LuLampDesk className="icons"/>
           <p>Keller</p>
         </div>
       </Link>
-      <Link to="/category/garten">
+      <Link to="/CategoryPage/garten">
         <div className="icon-container">
           <LuLampDesk className="icons"/>
           <p>Garten</p>
