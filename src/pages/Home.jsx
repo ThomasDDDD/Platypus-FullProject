93b064dfda042b59a7c8bf147fda7d17feb80963
Julian P <julian-peukert@web.de>
2024-10-14 13:14:05 +0200

    add: Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import { IconNavBar } from "../components/IconNavBar.jsx";
+import "./Home.css";
 
 function Home() {
   return (
     <>
+      <main>
+        <h2>MAKE YOUR</h2>
+        <h1>HOME BEATIFUL</h1>
+        <article><p>The best simple place where you discover most wonderful furnitures and make your home beatiful</p></article>
+        <button>Get Started</button>
+      </main>
       <IconNavBar />
     </>
   );
