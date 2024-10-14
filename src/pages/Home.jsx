025d99a375cd2fab371168cd884a71ca88b72700
Thomas D <thomas.dittrich@dci-student.org>
2024-10-14 10:23:09 +0200

    edit dies das

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import "../pages/Home.css";
+
+function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>hier könnte Ihre Werbung stehen</p>
+    </>
+  );
+}
+export default Home;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,17 @@
+const apiUrl = "http://localhost:7000";
+
+export const fetchData = async (endpoint) => {
+  if (!endpoint && typeof endpoint !== "string") {
+    throw new Error("a valid endpoint must be provided");
+  }
+  try {
+    return fetch(`${apiUrl}/${endpoint}`)
+      .then((response) => response.json())
+      .then((data) => {
+        return data;
+      });
+  } catch (e) {
+    console.error(e);
+    throw new Error(e);
+  }
+};
