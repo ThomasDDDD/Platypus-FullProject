e65a3a55f57b78078abdd6ecfe87c3e13d15e84d
Thomas D <thomas.dittrich@dci-student.org>
2024-10-14 15:11:47 +0200

    endOfDayCommit

--- src/components/SingleProductCard.jsx
+++ src/components/SingleProductCard.jsx
@@ -1,7 +1,12 @@
 function SingleProductCard({ product }) {
+  console.log(product);
   return (
     <div className="singleProductCardContainer">
+      <img src={product.image}></img>
       <h2>{product.title}</h2>
+      <p>{product.price}</p>
+      <p>{product.reviews}</p>
+      <p>{product.description}</p>
     </div>
   );
 }
